b7b071c113730bc6666a591817f21ea0e0775db6
Kally95 <Kally-95@hotmail.com>
2020-11-22 22:01:15 +0000

    Changed KtoC test to table driven test

--- main_test.go
+++ main_test.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestGetAPI(t *testing.T) {
-	k := GetAPIkey("APIKEY")
-	l := "London"
-	resp, err := GetAPI(l, k)
-	if err != nil {
-		t.Fatalf("GetAPI(%v, %v) = %v", l, k, resp)
-	}
-	fmt.Println(resp.Status)
-
-	// Currently returning a 401 (not authorised) *must fix*
-}
+// func TestGetAPI(t *testing.T) {
+// 	k := GetAPIkey("APIKEY")
+// 	l := "London"
+// 	resp, err := GetAPI(l, k)
+// 	if err != nil {
+// 		t.Fatalf("GetAPI(%v, %v) = %v", l, k, resp)
+// 	}
+// 	fmt.Println(resp.Status)
+// 	// Currently returning a 401 (not authorised) *must fix*
+// }
 
 func TestKtoC(t *testing.T) {
-	kelvin := 284.15
-	got := KtoC(kelvin)
-	want := "11.00"
-	if got != want {
-		t.Errorf("KtoC(%v) = %v; want = %v", kelvin, got, want)
+	testCases := []struct {
+		arg  float64
+		want string
+	}{
+		{284.15, "11.00"},
+		{295.20, "22.05"},
+		{289.63, "16.48"},
 	}
-
+	for _, tc := range testCases {
+		t.Logf("Testing: %v", tc.arg)
+		got := KtoC(tc.arg)
+		if got != tc.want {
+			t.Errorf("KtoC(%v) = %v; want %v", tc.arg, got, tc.want)
+		}
+	}
 }
